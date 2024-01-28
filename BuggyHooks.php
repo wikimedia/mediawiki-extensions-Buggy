42a9f83b19b7ad6ee0dfd1a49d83f8eaed74766a
zoranzoki21 <zorandori4444@gmail.com>
2024-01-28 09:07:06 +0100

    Add phpcs
    I tried my best to keep extension buggy as much as possible.
    
    Change-Id: Ie29e3f1bc066a826d206c4833dab5d34d82ff900

--- BuggyHooks.php
+++ BuggyHooks.php
@@ -7,6 +7,11 @@
  */
 
 class BuggyHooks {
+	/**
+	 * @param OutputPage &$out
+	 * @param Skin &$skin
+	 * @return bool
+	 */
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		static $fired = false;
 		if ( $fired ) {
@@ -19,7 +24,7 @@ class BuggyHooks {
 		if ( isset( $queryValues['buggy'] ) ) {
 			$buggy = $queryValues['buggy'];
 			if ( !is_array( $buggy ) ) {
-				$buggy = array( $buggy );
+				$buggy = [ $buggy ];
 			}
 
 			// PHP errors
@@ -32,24 +37,27 @@ class BuggyHooks {
 					case 'missing-class':
 						new Buggy_NoSuchClass();
 					case 'incompatible-class':
-						require( 'incompatible_class.php' );
+						require 'incompatible_class.php';
 					case 'php-error':
-						require( 'nosuchfile.php' );
+						require 'nosuchfile.php';
 						break;
 					case 'php-warning':
-						foreach ( null as $_ );
+						foreach ( null as $_ ) {
+
+						}
 						break;
 					case 'php-notice':
 						$_ = $doesNotExist;
 						break;
 					case 'sql':
 						$dbr = wfGetDB( DB_REPLICA );
+						// phpcs:ignore MediaWiki.Usage.DbrQueryUsage.DbrQueryFound
 						$dbr->query( 'THIS IS AN ERROR', __METHOD__ );
 				}
 			}
 
 			// JS/asset errors
-			$modules = array(
+			$modules = [
 				'css' => 'ext.Buggy.css',
 				'startup' => 'ext.Buggy.startup',
 				'setTimeout' => 'ext.Buggy.setTimeout',
@@ -59,10 +67,10 @@ class BuggyHooks {
 				'misc-cb' => 'ext.Buggy.misc-cb',
 				'mwload' => 'ext.Buggy.mwload',
 				'logError' => 'ext.Buggy.logError',
-			);
+			];
 			foreach ( $modules as $keyword => $module ) {
 				if ( in_array( $keyword, $buggy ) ) {
-					$out->addModules( array( $module ) );
+					$out->addModules( [ $module ] );
 				}
 			}
 		}
--- incompatible_class.php
+++ incompatible_class.php
@@ -1,7 +1,10 @@
 <?php
 
+// phpcs:ignore MediaWiki.Files.ClassMatchesFilename.NotMatch
 interface Foo {
 	public function foo();
 }
 
-class NotAFoo implements Foo {}
+// phpcs:ignore Generic.Files.OneObjectStructurePerFile.MultipleFound
+class NotAFoo implements Foo {
+}
